2bfebb4161228bcf398c01bdbce8a5423158c36c
IhorRohatiuk <rogatyuk60605@gmail.com>
2023-05-13 19:47:33 +0200

    Remake SetInfo() method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading; //for delay
 
 public class Set
 {
@@ -11,34 +12,78 @@ public class Set
         this.name = name;
     }
 
-    public void SetInfo()
+    public void ShowInfo()
     {
-        Console.WriteLine($"Set name: \'{name}\'");
-
-        Console.WriteLine($"Set questions: \n");
-        foreach (var i in Questions)
+        Console.WriteLine($"Set name: \'{name}\'\n");
+        
+        //Finding the longest QUESTION
+        string maxQuestion = Questions[0];
+        for (int i = 0; i < Questions.Length; i++)
         {
-            Console.WriteLine($"\t{i}");
+            if (Questions[i].Length > maxQuestion.Length)
+            {
+                maxQuestion = Questions[i];
+            }
         }
 
-        Console.WriteLine($"Set answers: \n");
-        foreach (var i in Answers)
+        //Finding the longest ANSWER
+        string maxAnswer = Answers[0];
+        for (int i = 0; i < Answers.Length; i++)
         {
-            Console.WriteLine($"\t{i}");
+            if (Answers[i].Length > maxAnswer.Length)
+            {
+                maxAnswer = Answers[i];
+            }
+        }
+
+        Console.Write("    Question:");
+
+        for (int j = 0; j < maxQuestion.Length - 9; j++)
+        {
+            Console.Write(" ");
+        }
+
+        Console.WriteLine("    Answer:");
+
+
+        Console.Write("    ");
+        for (int i = 0; i < maxQuestion.Length; i++)
+        {
+            Console.Write("─");
+        }
+        Console.Write("    ");
+        for (int i = 0; i < maxAnswer.Length; i++)
+        {
+            Console.Write("─");
+        }
+        Console.Write("\n");
+
+        for (int i = 0; i < Questions.Length; i++)
+        {
+            Console.Write($"    {Questions[i]}");
+
+            for (int j = 0; j < maxQuestion.Length - Questions[i].Length; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine($"    {Answers[i]}");              
         }
     }
 }
 
 internal class WordixConsoleApp
 {
-    public static void Main()
+    private static Set NewSet;
+
+    static void Main()
     {
         Console.Clear();
         
         StartScreen();
     }
 
-    public static void StartScreen()
+    static void StartScreen()
     {
         Console.Clear();
 
@@ -58,6 +103,7 @@ internal class WordixConsoleApp
                 TestList();
                 break;
             case ConsoleKey.Q:
+                Environment.Exit(0);
                 return;
             case ConsoleKey.I:
                 SetInfo();
@@ -69,51 +115,75 @@ internal class WordixConsoleApp
     }
 
     //Creating a new SET
-    public static void CreateSet()
+    static void CreateSet()
     {
         Console.Clear();
 
         //Addind a NAME
         Console.Write("Write a set name: ");
         string? name = Console.ReadLine();
-        Set NewSet = new Set(name); //?????Should that declaration be here?
+        NewSet = new Set(name); //?????Should that declaration be here?
 
-        while (true) // ?????????????Remake to DO-WHILE
+        while(true) 
         {
-            //Addind a question
-            Console.WriteLine("1) Add a question");
-            string GetQuestion = Console.ReadLine();
+            //[*]Info
+            Console.WriteLine("\n[!] Press \'+\' to add a question-answer block or \'S\' to exit");
 
-            Array.Resize(ref NewSet.Questions, NewSet.Questions.Length + 1);
-            NewSet.Questions[^1] = GetQuestion;
-
-            //Addind a answer
-            Console.WriteLine("2) Add an answer");
-            string GetAnswer = Console.ReadLine();
-
-            Array.Resize(ref NewSet.Answers, NewSet.Answers.Length + 1);
-            NewSet.Answers[^1] = GetAnswer;
-
-            //Exit control 
             ConsoleKeyInfo key = Console.ReadKey(true);
-            if (key.Key == ConsoleKey.Q)
-            { 
-                Main();
-                return;
+            switch (key.Key)
+            {
+                case ConsoleKey.OemPlus:
+
+                    //Addind a question
+                    Console.WriteLine("\n1) Add a question");
+                    string? GetQuestion = Console.ReadLine();
+
+                    Array.Resize(ref NewSet.Questions, NewSet.Questions.Length + 1);
+                    NewSet.Questions[^1] = GetQuestion;
+
+                    //Addind a answer
+                    Console.WriteLine("\n2) Add an answer");
+                    string? GetAnswer = Console.ReadLine();
+
+                    Array.Resize(ref NewSet.Answers, NewSet.Answers.Length + 1);
+                    NewSet.Answers[^1] = GetAnswer;
+
+                    break;
+
+                case ConsoleKey.S:
+                    Console.WriteLine("\n\n--Saved!--");
+                    Thread.Sleep(1000);
+                    Main();
+
+                    break;
+
+                default:
+                    continue;
             }
-        }
+            
+        } 
 
     }
 
     //Testing selected set
-    public static void TestList()
+    static void TestList()
     {
         Console.Clear();
         Console.WriteLine("now we will testing you =)");
     }
 
-    public static void SetInfo()
+    static void SetInfo()
     {
-        //
+        Console.Clear();
+
+        NewSet.ShowInfo();
+
+        ConsoleKeyInfo key = Console.ReadKey(true);
+        switch (key.Key)
+        {
+            default:
+                StartScreen();
+                break;
+        }
     }
 }
